e98d7f31629de806c826975024ca381239611f17
giatechconsumerwarehouse-crypto <giatechconsumerwarehouse@gmail.com>
2025-09-30 21:52:50 +0100

    Update firebase.js

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,8 +1,4 @@
-// scripts/firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
-
+// Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyD4rtE-D8HT4BxUjAz-opq1S2y8OeXzBXw",
   authDomain: "giatech-shipping.firebaseapp.com",
@@ -13,7 +9,5 @@ const firebaseConfig = {
   appId: "1:906996624627:web:9d8b8cfa4e210202009b86"
 };
 
-// Init
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getDatabase(app);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
