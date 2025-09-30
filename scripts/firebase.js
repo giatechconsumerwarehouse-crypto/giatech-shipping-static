e02cb56c558106f667ccd92880b3e64d2c85837c
giatechconsumerwarehouse-crypto <giatechconsumerwarehouse@gmail.com>
2025-09-30 20:56:57 +0100

    Update firebase.js

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,24 +1,19 @@
 // scripts/firebase.js
-// Modular Firebase init for Realtime Database + Auth (v9+)
-// Replace the firebaseConfig object with your actual values from Firebase Console > Project Settings
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD4rtE-D8HT4BxUjAz-opq1S2y8OeXzBXw",                 // <- replace if needed
+  apiKey: "AIzaSyD4rtE-D8HT4BxUjAz-opq1S2y8OeXzBXw",
   authDomain: "giatech-shipping.firebaseapp.com",
   databaseURL: "https://giatech-shipping-default-rtdb.firebaseio.com",
   projectId: "giatech-shipping",
-  storageBucket: "giatech-shipping.appspot.com",
+  storageBucket: "giatech-shipping.firebasestorage.app",
   messagingSenderId: "906996624627",
   appId: "1:906996624627:web:9d8b8cfa4e210202009b86"
 };
 
+// Init
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-const auth = getAuth(app);
-
-// Export so other scripts can import: `import { db, auth } from './firebase.js'`
-export { db, auth };
+export const auth = getAuth(app);
+export const db = getDatabase(app);
